3f027d86d38e8cfd27b543e703f6c78e26e9506c
lssdg <lssdannygiftson@gmail.com>
2023-03-24 21:21:03 +0530

    reduxcart

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,19 @@
+import classes from './CartButton.module.css';
+import uiSlice from '../../store/uiSlice';
+import { uiActions } from '../../store/uiSlice';
+import { useDispatch } from 'react-redux';
+
+const CartButton = (props) => {
+  const dispatch=useDispatch();
+  const toggleCart=()=>{
+    dispatch(uiActions.toggle());
+  }
+  return (
+    <button className={classes.button} onClick={toggleCart}>
+      <span>My Cart</span>
+      <span className={classes.badge}>1</span>
+    </button>
+  );
+};
+
+export default CartButton;
---
+++ src/store/cartSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+const cartSlice = createSlice({
+    name:'cart',
+    initialState:{
+        items:[],
+        totalQuantity:0,
+        totalAmount:0
+    },
+    reducers:{
+        addItemToCart(state,action){
+            const newitem=action.payload;
+            const existingItem = state.items.find(item=>item.id===newitem.id);
+            if(!existingItem){
+                state.items.push({itemId:newitem.id,price:newitem.price,quantity:1,totalPrice:newitem.price,title:newitem.title});
+            }else{
+                existingItem.quantity++;
+                existingItem.totalPrice=existingItem.totalPrice+newitem.price;
+            }
+        },
+        removeItemFromCart(state,action){
+            const id=action.payload;
+            const existingItem=state.items.find(item=>item.id===id);
+            if(existingItem.quantity===1){
+                state.items=state.items.filter(item=>item.id);
+            }else{
+                existingItem.quantity--;
+                existingItem.totalPrice=existingItem.totalPrice-existingItem.price;
+            }
+        }
+    }
+})
+export const cartActions=cartSlice.actions;
+export default cartSlice
